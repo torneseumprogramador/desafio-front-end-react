d088743773eeba907820731996192bedf6fe6f40
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-07-06 07:04:37 -0300

    versão inicial react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/style.scss';
+import logo from './assets/imagens/logo.webp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="row">
+      <header className="col-12 row">
+        <div className="col-1">
+          <img src={logo} alt='Logo do projeto espacial' />
+        </div>
+        <nav className="col-11">
+          <ul>
+            <li>Home</li>
+            <li>Cadastro</li>
+            <li>Sobre</li>
+            <li>Login</li>
+          </ul>
+        </nav>
       </header>
+      <main className="col-12 row">
+        <section className="col-12 col-md-8">
+          conteudo
+        </section>
+        <aside  className="col-12 col-md-4">
+          itens lateral
+        </aside>
+      </main>
+      <footer className="col-12">
+      footer
+      </footer>
     </div>
   );
 }
