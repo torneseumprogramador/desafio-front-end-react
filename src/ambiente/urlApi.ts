1f8428ce240805753ed3e15e38b04199d0102d4a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-07-12 07:44:58 -0300

    aula 40 typescript fetch

---
+++ src/ambiente/urlApi.ts
@@ -0,0 +1,2 @@
+const urlApi:string = "http://localhost:3001";
+export default urlApi;
---
+++ src/models/produto.ts
@@ -0,0 +1,12 @@
+type Produto = {
+    id: number;
+    nome: string;
+    preco: number;
+    descricao: string;
+    data_criacao: Date;
+    data_modificacao: Date;
+    quantidade_estoque: number;
+};
+
+export default Produto;
+  
